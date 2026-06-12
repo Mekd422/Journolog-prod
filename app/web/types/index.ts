d9544efba6af7014f871cfa18364c2598e797fbc
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2026-06-12 23:00:41 +0300

    updated

--- app/web/types/index.ts
+++ app/web/types/index.ts
@@ -7,6 +7,8 @@ export interface User extends RecordModel {
   name: string;
   email: string;
   avatar?: string;
+  bio?: string;
+  public_profile_slug?: string;
 }
 
 export interface JourneyLog extends RecordModel {
@@ -19,20 +21,41 @@ export interface JourneyLog extends RecordModel {
   slug: string;
   status: JourneyLogStatus;
   user: string;
+  is_featured?: boolean;
+  tags?: string[];
+}
+
+export interface MapboxLocation {
+  name: string;
+  latitude: number;
+  longitude: number;
 }
 
 export interface Entry extends RecordModel {
   title: string;
   entry_date: string;
+  end_date?: string;
+  location_name?: string;
   location?: string;
   show_on_map?: boolean;
   content?: Record<string, unknown>;
   status: EntryStatus;
   journey_log: string;
   user: string;
+  latitude?: number;
+  longitude?: number;
 }
 
 export interface Media extends RecordModel {
   file: string;
   user: string;
+  caption?: string;
+  location_name?: string;
+  journey_log?: string;
+}
+
+export interface Tag extends RecordModel {
+  name: string;
+  type: "style" | "region" | "activity" | "mood";
+  user?: string;
 }
